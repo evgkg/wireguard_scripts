16aab3a769edfa3de5b2a2eaeb8cfd6e8f7eaf23
root <root@v1814257.hosted-by-vdsina.ru>
2023-04-15 08:33:38 +0300

    refactoring

--- newclient.sh
+++ newclient.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
 cd /etc/wireguard
+
+CONFIGFILE="wg0.conf"
+DEFAULTIP="10.0.0."
+MASK="/32"
+
+GREEN='\033[0;32m'
+NC='\033[0m'
+
 wg genkey | tee $1_privatekey | wg pubkey > $1_publickey
 
 pbkey=$(cat $1_publickey)
 prkey=$(cat $1_privatekey)
 
-configFile="wg0.conf"
-
-defaultIp="10.0.0."
-mask="/32"
-ipLS=$(cat $configFile | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' | tail -1 | cut -d . -f 4)
+ipLS=$(grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' $CONFIGFILE | tail -1 | cut -d . -f 4)
 ipLS=$(($ipLS + 1))
 
-resultIp="$defaultIp$ipLS$mask"
+resultIp="$DEFAULTIP$ipLS$MASK"
 
-echo "" >> $configFile
-echo "#$1" >> $configFile
-echo "[Peer]" >> $configFile
-echo "PublicKey = $pbkey" >> $configFile
-echo "AllowedIPs = $resultIp" >> $configFile
+echo "" >> $CONFIGFILE
+echo "#$1" >> $CONFIGFILE
+echo "[Peer]" >> $CONFIGFILE
+echo "PublicKey = $pbkey" >> $CONFIGFILE
+echo "AllowedIPs = $resultIp" >> $CONFIGFILE
+
+echo -e "${GREEN}$1 privatekey:${NC}\t $prkey"
+echo -e "${GREEN}$1 ip:${NC}\t\t $resultIp"
+echo ""
 
 systemctl restart wg-quick@wg0
 systemctl status wg-quick@wg0 | head -n 3
-echo ""
-
-GREEN='\033[0;32m'
-NC='\033[0m'
-echo -e "${GREEN}privatekey:${NC}\t $prkey"
-echo -e "${GREEN}ip:${NC}\t\t $resultIp"
